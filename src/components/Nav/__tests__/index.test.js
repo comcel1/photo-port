4839496770086f46d0c7f9cc568ffac2c354bc69
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-05-11 20:43:43 -0500

    added nav tests

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+//This will ensure that after each test, we won't have any leftover memory data that could give us false results.
+afterEach(cleanup);
+
+describe('Nav component', () => {
+  // First Test
+  it('renders', () => {
+    render(<Nav />);
+  });
+  // Snapshot Test
+  it('matches snapshot', () => {
+    // render About
+    const { asFragment } = render(<Nav />);
+    // assert value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+//the describe function is not absolutely necessary for the test to run; it is used to organize tests into sections that are typically labeled with the element being tested.
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+    // Arrange
+    const { getByLabelText } = render(<Nav />);
+    // Assert
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+});
+
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
+    // Arrange
+    const { getByTestId } = render(<Nav />);
+    // Assert
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+});
