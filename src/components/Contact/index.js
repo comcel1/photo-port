0410ffaa71165fcbc148d2bd5f7e645a4b306e82
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-05-16 15:57:53 -0500

    added contact form tests

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -37,7 +37,7 @@ function ContactForm() {
 
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -71,7 +71,9 @@ function ContactForm() {
             <p className="error-text">{errorMessage}</p>
           </div>
         )}
-        <button type="submit">Submit</button>
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,6 +8,8 @@ const categories = [
 ];
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 //This will ensure that after each test, we won't have any leftover memory data that could give us false results.
 afterEach(cleanup);
@@ -15,7 +17,15 @@ afterEach(cleanup);
 describe('Nav component', () => {
   // First Test
   it('renders', () => {
-    render(<Nav />);
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+      />
+    );
   });
   // Snapshot Test
   it('matches snapshot', () => {
